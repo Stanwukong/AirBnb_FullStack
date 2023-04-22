e4f2261db7adf73c30e49d7999ddffb9d2427407
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-04-22 13:15:24 +0100

    Initial commit: Environment setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className='text-rose-500 text-2xl'>Hello Airbnb</div>
+  )
+}
