c5211f4c291c4bea3ccf6262876f90804ff9313d
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-05-10 16:22:29 +0100

    Force Dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'auto';
+// 'auto' | 'force-dynamic' | 'error' | 'force-static'
+
 
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingParams } from "./actions/getListings";
