c159063addf788d945b6c268a02fa1b5ef17f31d
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-05-10 15:39:25 +0100

    I hate TypeScript

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -1,51 +1,49 @@
-'use client'
+"use client"
 
-import { FieldValues, 
-	FieldErrors,
-	UseFormRegister } from "react-hook-form";
-import { BiDollar } from "react-icons/bi";
+import { FieldValues, FieldErrors, UseFormRegister } from "react-hook-form"
+import { BiDollar } from "react-icons/bi"
 
 interface InputProps {
-	id: string;
-	label: string;
-	type?: string;
-	disabled?: boolean
-	formatPrice?: boolean;
-	required?: boolean;
-	register?: UseFormRegister<FieldValues>
-	errors?: FieldErrors
+  id: string
+  label: string
+  type?: string
+  disabled?: boolean
+  formatPrice?: boolean
+  required?: boolean
+  register: UseFormRegister<FieldValues>
+  errors: FieldErrors
 }
 
-const Input:React.FC<InputProps> = ({
-	id,
-	label,
-	type,
-	disabled,
-	formatPrice,
-	required,
-	register,
-	errors
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type,
+  disabled,
+  formatPrice,
+  required,
+  register,
+  errors,
 }) => {
-	return ( 
-		<div className="w-full relative">
-			{formatPrice && (
-				<BiDollar 
-					size={24}
-					className="
+  return (
+    <div className="w-full relative">
+      {formatPrice && (
+        <BiDollar
+          size={24}
+          className="
 						text-neutral-700
 						absolute
 						top-5
 						left-2
 					"
-				/>
-			)}
-			<input 
-				id={id}
-				disabled={disabled}
-				{ ... register(id, { required }) }
-				placeholder=" "
-				type={type}
-				className={`
+        />
+      )}
+      <input
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=" "
+        type={type}
+        className={`
 					peer
 					w-full
 					p-4
@@ -58,13 +56,13 @@ const Input:React.FC<InputProps> = ({
 					transition
 					disabled:opacity-70
 					disabled:cursor-not-allowed
-					${formatPrice ? 'pl-9' : 'pl-4'}
-					${errors[id] ? 'border-red-500' : 'border-neutral-300'}
-					${errors[id] ? 'focus:border-red-500' : 'focus:border-black'}
+					${formatPrice ? "pl-9" : "pl-4"}
+					${errors[id] ? "border-red-500" : "border-neutral-300"}
+					${errors[id] ? "focus:border-red-500" : "focus:border-black"}
 				`}
-			/>
-			<label
-				className={`
+      />
+      <label
+        className={`
 					absolute
 					text-md
 					duration-150
@@ -73,18 +71,18 @@ const Input:React.FC<InputProps> = ({
 					top-5
 					z-10
 					origin-[0]
-					${formatPrice ? 'left-9' : 'left-4'}
+					${formatPrice ? "left-9" : "left-4"}
 					peer-placeholder-shown:scale-100
 					peer-placeholder-shown:translate-y-0
 					peer-focus:scale-75
 					peer-focus:-translate-y-4
-					${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
+					${errors[id] ? "text-rose-500" : "text-zinc-400"}
 				`}
-			>
-				{label}
-			</label>
-		</div>
-	 );
+      >
+        {label}
+      </label>
+    </div>
+  )
 }
- 
-export default Input;	
+
+export default Input
